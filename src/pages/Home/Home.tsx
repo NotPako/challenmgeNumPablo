6648f16ce21bb8b728d21d569243dc4aff49db33
PABLO JUAN GRAU <pabjuagr@upv.edu.es>
2025-05-02 19:17:39 +0200

    style explorer page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,10 +8,13 @@ import './Home.css'
 const Home: React.FC = () => {
   return (
     <>
-      <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}> 
+      <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' ,backgroundColor:'rgb(7, 26, 61)', minHeight:'100vh'}}> 
+        <div style={{marginTop:'5rem', display: 'flex', flexDirection: 'column', alignItems: 'center', color:'white'}}>
         <h1>Welcome to Globify</h1>
         <p>Explore the globe thanks to a fantastic API</p>
         <Catalogue/>
+        </div>
+        
       </div>
     </>
   );
