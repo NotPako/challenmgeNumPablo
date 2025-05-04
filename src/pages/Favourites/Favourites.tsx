b0b3a4000f843027fa9551e27c4936cdc1f0be76
PABLO JUAN GRAU <pabjuagr@upv.edu.es>
2025-05-04 20:16:50 +0200

    add nav button in favorites page

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import CountryCard from '../../components/CountryCard/CountryCard';
 import { useFavorites } from '../../Providers/FavoritesProvider';
+import { useNavigate } from 'react-router-dom';
 import './Favourites.css';
 
 
@@ -27,6 +28,7 @@ interface Country {
 export default function Favourites() {
     const [countries, setCountries] = useState<Country[]>([]);
     const { favorites } = useFavorites();
+    const navigate = useNavigate();
 
     useEffect(() => {
       setCountries(favorites);
@@ -40,6 +42,9 @@ export default function Favourites() {
       <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '5rem', color: 'white' }}> 
         <h1>My Favorites</h1>
         <p>These are the countries you marked as favorites</p>
+        <p> <button className="explore-button" onClick={() => navigate('/Explore')}>
+        Back to Explorer
+      </button></p>
       </div>
     <div style = {{padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}> 
        <div className="countryGridDesign">
