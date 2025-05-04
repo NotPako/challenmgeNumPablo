e8c86070c64c9fa95681f0a0b31085c90c970b2e
PABLO JUAN GRAU <pabjuagr@upv.edu.es>
2025-05-04 20:12:58 +0200

    add animation for transition

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {motion} from 'framer-motion'
 import Catalogue from '../Catalogue/Catalogue';
 import './Home.css'
 
@@ -8,14 +9,20 @@ import './Home.css'
 const Home: React.FC = () => {
   return (
     <>
-      <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' ,backgroundColor:'rgb(7, 26, 61)', minHeight:'100vh'}}> 
+    
+      <motion.div 
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.4 }}
+        style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' ,backgroundColor:'rgb(7, 26, 61)', minHeight:'100vh'}}> 
         <div style={{marginTop:'5rem', display: 'flex', flexDirection: 'column', alignItems: 'center', color:'white'}}>
-        <h1>Welcome to Globify</h1>
-        <p>Explore the globe thanks to a fantastic API</p>
+        <h1>Explorer</h1>
+        <p>Find your country and click on it to find more details</p>
         <Catalogue/>
         </div>
         
-      </div>
+      </motion.div>
     </>
   );
 };
